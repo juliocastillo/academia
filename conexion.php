63105be3b01a02bb70c413576401e5a7341849e7
julio <jcastillore@yaoo.es>
2018-01-29 23:29:31 -0600

    agregando config.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,6 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "academia");
+require "config.php";
+$mysqli = new mysqli($host, $user, $password, $database);
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
